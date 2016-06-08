4e5603a756d12fe9eefab86a757eb6fb1d4c6598
Nick P Smith <npsmith1990@gmail.com>
2016-06-08 17:57:02 -0500

    show remaining time in a udemy course

---
+++ popup.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  var total = 0;
+  var checkReady = function() {
+    if (typeof $('.sidebar--nav') === 'undefined' || $('.sidebar--nav').length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  var interval = setInterval(function(){
+    if (checkReady()) {
+      clearInterval(interval)
+      setTimeout(function(){
+        $('.lecture__item').not('.completed').map(function(i,el){
+          const rawTime = $(el).find('.lecture__item__link__time').html();
+          var timeArr = rawTime.split(':');
+          var min = +timeArr[0];
+          var sec = +timeArr[1];
+          sec = sec / 60;
+          min += sec;
+          total += min;
+        });
+        var minutes = ((total % 60) + "").split('.')[0];
+        var hours = ((total / 60) + "").split('.')[0];
+        var string = `${hours} hours and ${minutes} minutes remaining in this course`;
+        console.log(string);
+
+        $('.curriculum-navigation .panel-group').prepend(
+          '<div class="panel curriculum-navigation__section">' +
+          '<div class="panel-heading">' + 
+          '<div class="curriculum-navigation__section__title">' + 
+          string +
+          '</div>' +
+          '</div>' +
+          '</div>'
+        )
+      }, 2000);
+
+    }
+  });  
+});
